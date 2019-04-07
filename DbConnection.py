2ce68f36de945e76cb530fdf25ba17f745b3323a
Manviksagar <aallu.vidyasagar@gmail.com>
2019-04-07 15:36:07 +0530

    Initial commit

---
+++ DbConnection.py
@@ -0,0 +1,13 @@
+import pymysql
+
+# database connection
+connection = pymysql.connect(host="localhost", user="root", passwd="", database="sag")
+cursor = connection.cursor()
+retrive = "Select * from sag_ta;"
+
+# executing the quires
+cursor.execute(retrive)
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+connection.close()
