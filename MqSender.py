d22d24236c51fe5fffe46142a8b201e7857ad275
Manviksagar <aallu.vidyasagar@gmail.com>
2020-05-05 12:12:59 +0530

    New Concepts jan21

--- MqSender.py
+++ MqSender.py
@@ -4,9 +4,9 @@ import pika
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
-
 channel.queue_declare(queue='hello')
-
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World2!')
-print(" [x] Sent 'Hello World 2!'")
+# The default exchange is implicitly bound to every queue, with a routing key equal to the queue name.
+# It is not possible to explicitly bind to, or unbind from the default exchange. It also cannot be deleted.
+channel.basic_publish(exchange='', routing_key='hello', body='Hello World8!')
+print(" [x] Sent 'Hello World 55558885!'")
 connection.close()
